c455c5de847c7356353b6f556bad120aa2b7e08a
Jagdeep Panta <jagdeep@veridic.local>
2023-11-29 20:39:26 +0530

    Initial Commit

---
+++ Test-Taste-Console-Application/Domain/DataTransferObjects/PlanetDto.cs
@@ -0,0 +1,16 @@
+﻿using System.Collections.Generic;
+using System.Linq;
+using Newtonsoft.Json;
+
+namespace Test_Taste_Console_Application.Domain.DataTransferObjects
+{
+    public class PlanetDto
+    {
+        public string Id { get; set; }
+        public float SemiMajorAxis { get; set; }
+        public ICollection<MoonDto> Moons { get; set; }
+       
+
+
+    }
+}
---
+++ Test-Taste-Console-Application/Domain/Objects/Planet.cs
@@ -0,0 +1,53 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Collections.ObjectModel;
+using System.Linq;
+using Test_Taste_Console_Application.Domain.DataTransferObjects;
+
+namespace Test_Taste_Console_Application.Domain.Objects
+{
+    public class Planet
+    {
+        public string Id { get; set; }
+        public float SemiMajorAxis { get; set; }
+        public ICollection<Moon> Moons { get; set; }
+        
+        public float AverageMoonGravity
+        {
+            get
+            {
+                if (Moons != null && Moons.Any())
+                {
+                    // Calculate the average gravity based on the Moons collection
+                    float totalGravity = Moons.Sum(moon => moon.Gravity);
+                    return totalGravity / Moons.Count;
+                }
+                else
+                {
+                    // Return a default value or handle the case where there are no moons
+                    return 0.0f;
+                }
+            }
+        }
+
+        public Planet(PlanetDto planetDto)
+        {
+            Id = planetDto.Id;
+            SemiMajorAxis = planetDto.SemiMajorAxis;
+            Moons = new Collection<Moon>();
+            if(planetDto.Moons != null)
+            {
+                foreach (MoonDto moonDto in planetDto.Moons)
+                {
+                    Moons.Add(new Moon(moonDto));
+                }
+            }
+        }
+
+        public Boolean HasMoons()
+        {
+            return (Moons != null && Moons.Count > 0);
+        }
+       
+    }
+}
